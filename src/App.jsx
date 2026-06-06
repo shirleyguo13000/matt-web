d1da2d9ec2cb9235056c079be423399d52dc2a1d
shirleyguo13000 <shirleyguo13000@gmail.com>
2026-06-05 22:57:01 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
